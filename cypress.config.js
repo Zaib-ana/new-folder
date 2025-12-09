9fa663f2434a567ca5afdf2481c977a070a68358
Zaib-ana <zaibunisadab@gmail.comL>
2025-12-09 09:06:44 +0500

    Updated cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://stg.platform.creatingly.com/apps/",
+    chromeWebSecurity: false,
+    experimentalModifyObstructiveThirdPartyCode: true,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
