0fe66aed6209c6c7cfe5503808c35448c576f2d5
Zaib-ana <zaibunisadab@gmail.comL>
2025-12-12 13:22:07 +0500

    Fixed Cypress config and added drag-drop commands

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://stg.platform.creatingly.com/",
-    chromeWebSecurity: false,
-    experimentalModifyObstructiveThirdPartyCode: true,
-
-    // <-- Add these two lines
-    supportFile: "cypress/support/commands.js", // path to your support file from root
-    specPattern: "cypress/e2e/**/*.js",        // adjust if your tests are inside 1getting-started/
-
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    supportFile: "cypress/support/commands.js"
+  }
 });
