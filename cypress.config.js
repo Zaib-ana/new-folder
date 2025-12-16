0e5397f2ec173e8acdcc9efc0463d1fb396fd24d
Zaib-ana <zaibunisadab@gmail.comL>
2025-12-16 08:59:49 +0500

    Updated Cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // if you want to disable support file
-    supportFile: false,
-    // or use default:
-    // supportFile: "cypress/support/e2e.js"
-  }
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
